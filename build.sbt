dbe26c155cc88ec5225ddb2c382223fa2f5deeeb
Hryhorii Prokopiv <hprok@softserveinc.com>
2017-12-30 22:35:25 +0200

    hello word akka http

--- build.sbt
+++ build.sbt
@@ -3,3 +3,8 @@ name := "akkaHello"
 version := "0.1"
 
 scalaVersion := "2.12.4"
+
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-http"   % "10.1.0-RC1",
+  "com.typesafe.akka" %% "akka-http-testkit" % "10.0.11" % Test,
+"com.typesafe.akka" %% "akka-stream" % "2.5.8")
---
+++ src/main/scala/AkkaHelloWord.scala
@@ -0,0 +1,39 @@
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives._
+import akka.stream.ActorMaterializer
+import scala.io.StdIn
+import akka.http.scaladsl.server.{RouteResult}
+
+
+object AkkaHelloWord {
+
+  def main(Args: Array[String]): Unit = {
+    implicit val actorSystem = ActorSystem("awesomeSystem")
+    implicit val materialize = ActorMaterializer()
+    implicit val executionContext = actorSystem.dispatcher
+
+
+    val routes = Routes.getRoutes
+    val bindingFuture = Http().bindAndHandle(routes, "localhost", 8080)
+    StdIn.readLine()
+    bindingFuture
+      .flatMap(_.unbind())
+      .onComplete(_ => actorSystem.terminate())
+  }
+}
+
+object Routes {
+  def getRoutes(implicit
+                materializer: ActorMaterializer,
+                actorSystem: ActorSystem) = {
+
+    val route = path("hello") {
+      get {
+        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka http</h1>"))
+      }
+    }
+    RouteResult.route2HandlerFlow(route)
+  }
+}
